58137b66fcd663d2970629ec2080bcae032c92a7
antillonk <antillonk@gmail.com>
2026-09-24 15:59:16 -0400

    Update nyc_bronze.sql

--- NYC_API_INGESTION/dbt_nyc_transformations/databricks_NYC_API_Project/models/staging/nyc_bronze.sql
+++ NYC_API_INGESTION/dbt_nyc_transformations/databricks_NYC_API_Project/models/staging/nyc_bronze.sql
@@ -1,5 +1,6 @@
 {{ config(materialized='table') }}
 
-
+-- updated due source due to productionized append ingestion for batch history
 select *
-from  {{ source('nyc_api_source_data', 'nyc_api_service_request') }}
+-- from  {{ source('nyc_api_source_data', 'nyc_api_service_request') }}
+from  {{ source('nyc_api_source_data', 'nyc_api_service_request_02') }}
